37e2e39fe0a1378516f1a2be1ce594e5816b5af4
Niklas Rudemo <niklas@rudemo.com>
2013-07-25 09:21:06 +0000

    web.js

--- web.js
+++ web.js
@@ -3,9 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-    var fs = require('fs');
-    var mbuffer = fs.readFileSync("index.html");
-    response.send(mbuffer.toString('utc-8', fs.readFileSync("index.html")));
+    var fs1 = require('fs');
+    var mbuffer = fs1.readFileSync("index.html");
+    response.send(mbuffer.toString('utc-8', fs1.readFileSync("index.html")));
 });
 
 var port = process.env.PORT || 5000;
