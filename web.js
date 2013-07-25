58b48a0dafd131ae0df2086a2644a0d927bf450f
Niklas Rudemo <niklas@rudemo.com>
2013-07-25 10:28:13 +0000

    web.js

--- web.js
+++ web.js
@@ -4,9 +4,17 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
     var fs = require('fs');
-    var buffer = fs.readFileSync("index.html");
-    buffer.toString('utf-8');
-    response.send(buffer);
+    var FILE_DEFAULT = "index.html";
+    file = file || FILE_DEFAULT;
+    var buffer = new Buffer(fs.readFileSync(file));
+    sometext = '<h1>Hello world3!<h1>';
+    app.get('/',function(request, response) {
+	response.send(sometext);
+});
+
+//    var buffer = fs.readFileSync("index.html");
+//    buffer.toString('utf-8');
+//    response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
