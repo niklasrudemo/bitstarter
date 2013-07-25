b215d660585efc8c0379682a31bae8923bcb173f
Niklas Rudemo <niklas@rudemo.com>
2013-07-25 12:46:26 +0000

    web.js

--- web.js
+++ web.js
@@ -6,7 +6,7 @@ app.get('/', function(request, response) {
     var fs = require('fs');
     var mbuffer = fs.readFileSync("index.html");
     var testbuf = new Buffer("Hello World4!","utf-8");
-    testbuf.write(testbuf);
+    testbuf.write("Hellow World4!");
 //    response.send(mbuffer.toString('utf-8', fs.readFileSync("index.html")));
 });
 
